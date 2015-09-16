d0b7ba99ec304a5e0cc80ab4f84420789d83ce3c
Naman Priyadarshi <namanpri2@gmail.com>
2015-09-16 11:21:35 -0700

    Update foobar.rb
    Completed Q4 on HW1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,17 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(arr)
+  	total = 0
+  	arrToInt = arr.map {|x| x.to_i}
+  	plusTwoArr = arrToInt.map {|x| x+2}
+    array = Array.new
+  	for i in (0...plusTwoArr.length) do
+      if ((plusTwoArr[i])%2==0 && (plusTwoArr[i])<10)
+  		  array.push(plusTwoArr[i])
+      end
+  	end
+  	plusTwoArrUniq = array.uniq
+  	plusTwoArrUniq.each {|x| total+= x}
+  	return total
+  end
 end
